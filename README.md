48a9ffb37c6dd15c8c46df2c6a5ff6b762bc30fd
Yehia Khalaf <yehia-khalaf@hotmail.com>
2019-09-25 19:55:49 +0200

    Created initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,14 @@
+/*
+ * Author: Yehia Khalaf Abdelaal
+ * Email: yehia-khalaf@hotmail.com
+ * Cairo, Egypt.
+ * This project is made to calculate some basic statistical data for a given
+ * array with determined size. as an assignment for Embedded systems course.
+ * Files: stats.c - stats.h
+ * Use: open stats.c and enter the elements of the data array and its size.
+ * 	Complie and run the program using GNU GCC.
+ * 	Output: print out of the data array elements, and the basic statistical
+ * 	data (Maximum, Minimum, Mean, Median).
+ * Date: 25/9/2019
+ */
+  
---
+++ stats.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file Stats.h
+ * @brief
+ * This project is made to calculate some basic statistical data for a given
+ * array with determined size. as an assignment for Embedded systems course.
+ * Use: Enter the elements of the data array and its size, Complie and run the
+ * 	program using GNU GCC.
+ * Output: print out of the data array elements, and the basic statistical
+ * 	data (Maximum, Minimum, Mean, Median).
+ *
+ * @author Yehia Khalaf
+ * @date 25/9/2019
+ *
+ */
+
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
